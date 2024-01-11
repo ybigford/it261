3b5a0ca9e51f17bc327d936f037e43b8b077d6c7
Yapa Bigford <110362211+ybigford@users.noreply.github.com>
2024-01-10 20:33:41 -0800

    Add files via upload

---
+++ weeks/week2/var.php
@@ -0,0 +1,23 @@
+<?php
+// my variables
+
+
+$name = "Yapa"; 
+$name = "Wagner";
+echo $name;
+
+// if we declare the same variable several times the last value that was assigned is the only value displayed
+
+
+$body_temp = 98;
+$body_temp_new = 98.6;
+echo "br";
+// echo "br";
+echo $body_temp;
+echo "br";
+echo $body_temp_new;
+
+echo "br";
+echo "My name is $name!";
+echo "br";
+echo 'My name is $name!';
