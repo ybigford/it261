574cfa9d9e1299bb5c8261c420e0366c533cf5ec
Yapa Bigford <110362211+ybigford@users.noreply.github.com>
2024-02-01 18:25:58 -0800

    Add files via upload

--- weeks/week5/currency2.php
+++ weeks/week5/currency2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Currency 1 php form</title>
+    <title>Currency 2 form with drop down (select and option)</title>
     <link href="css/styles.css" type="text/css" rel="stylesheet">
 </head>
 <body>
@@ -31,6 +31,21 @@
 
     </ul>
 
+    <label>Choose your banking institution</label>
+    <select name="bank">
+        <option value="" NULL>Select one!</option>
+        <option value="boa">Bank of America</option>
+        <option value="chase">Chase Bank</option>
+        <option value="banner">Banner Bank</option>
+        <option value="wells">Wells Fargo</option>
+        <option value="becu">Boeing Credit Union</option>
+
+
+
+
+
+    </select>
+
    <input type="submit" value="Convert it">
 
    <p><a href="">Reset it!</a></p>
@@ -45,26 +60,50 @@
   // if the fields are not empty, we will elseif they are set
 
   if($_SERVER['REQUEST_METHOD'] == 'POST') {
-      if(empty($_POST['name'] &&
-      $_POST['email'] &&
-      $_POST['amount'] &&
-      $_POST['currency'])) {
+      if(empty($_POST['name'] )) {
 
-          echo '<p class="error">Please fill out all of the fields</p>';
-      } elseif (isset($_POST['name'],
+          echo '<p class="error">Please fill out your name!</p>';
+    } 
+    
+    
+    if(empty($_POST['email'] )) {
+
+        echo '<p class="error">Please fill out your email!</p>';
+    } 
+    
+    if(empty($_POST['amount'] )) {
+
+        echo '<p class="error">Please fill out your amount!</p>';
+    } 
+
+    if(empty($_POST['currency'] )) {
+
+        echo '<p class="error">Please check your currency!</p>';
+  } 
+
+  if ($_POST['bank'] == NULL) {
+
+    echo '<p class="error">Please choose your banking institution!</p>';
+} 
+
+
+    if (isset($_POST['name'],
       $_POST['email'],
       $_POST['amount'],
-      $_POST['currency'] )) {
+      $_POST['currency'],
+      $_POST['bank'] )) {
       $name = $_POST['name'];
       $email = $_POST['email'];
       $amount = $_POST['amount'];
       $currency = $_POST['currency'];
+      $bank = $_POST['bank'];
       $dollars = $amount * $currency;
 
       echo '
       <div class="box">
       <h2>Hello '.$name.'</h2>
-      <p>You now have <b> $'.floor($dollars).' American dollars</b> and we will be emailing you at '.$email.' with your information!</p>
+      <p>You now have <b> $'.floor($dollars).' American dollars</b> and we will be emailing you at <b> '.$email.' </b> with your information, as well as depositing your funds at <b>
+      '.$bank.' bank!</b></p>
 
       </div>
       
