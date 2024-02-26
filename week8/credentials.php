1086c5b2736bcf966b2be54fbbe1c33f3023f4c7
Yapa Bigford <110362211+ybigford@users.noreply.github.com>
2024-02-26 00:44:09 -0800

    Delete week8 directory

--- week8/credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// My credentials that will allow me to connect to the database!!!
-
-define('DB_NAME', 'ybigford_winter2024');
-define('DB_USER', 'ybigford');
-define('DB_PASSWORD', 'Thammasat36!');
-define('DB_HOST', 'mysql.ybigford.com');
